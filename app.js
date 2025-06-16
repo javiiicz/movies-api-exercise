268e9d384f9e1af3702eeb04029f943fcb429e62
Elikem Kuivi <elikem@meta.com>
2025-06-16 08:51:11 -0700

    Add welcome route

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ var express = require("express");
 var app = express();
 const records = require("./records.js");
 
+app.get("/", (req, res) => {
+  res.send("Welcome to movies API");
+});
+
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
 });
