97164e22d70aaed31cb3170cb7d3808c7af5d11e
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-16 12:51:14 -0700

    [Backend] Add post movies endpoint

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require("express");
 var app = express();
 const records = require("./records.js");
 
+app.use(express.json())
+
 app.get("/", (req, res) => {
   res.send("Welcome to movies API");
 });
@@ -17,6 +19,11 @@ app.get("/movies/:id", async (req, res) => {
   res.send(movies)
 })
 
+app.post('/movies', async (req, res) => {
+  const newMovie = req.body
+  const added = await records.createMovie(newMovie);
+  res.send(added)
+})
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
