b9d7cdf3f3ec975201a5c8ad98dbc15a92a0fb78
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-16 12:39:16 -0700

    [Backend] Get movie endpoints

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ app.get("/", (req, res) => {
   res.send("Welcome to movies API");
 });
 
+app.get("/movies", async (req, res) => {
+  const movies = await records.getMoviesList()
+  res.send(movies);
+})
+
+app.get("/movies/:id", async (req, res) => {
+  const id = req.params.id
+  const movies = await records.getMovieById(id);
+  res.send(movies)
+})
+
+
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
 });
